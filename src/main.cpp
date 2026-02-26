f4ce5d4cf5cf6220619d468d836775dc7cd3e690
thausoma <r0r0r00r0r0r8@mail.ru>
2026-02-27 03:44:53 +0700

    initial commit: basic graphics logic, zmq mutex and submodules for imgui/implot

---
+++ src/main.cpp
@@ -0,0 +1,165 @@
+#include <GL/glew.h>
+#include <SDL2/SDL.h>
+#include <iostream>
+#include <chrono>
+#include <thread>
+#include <cmath>
+#include <mutex>
+#include <string>
+#include <zmq.hpp> 
+
+#include "backends/imgui_impl_opengl3.h"
+#include "backends/imgui_impl_sdl2.h"
+#include "imgui.h"
+#include "implot.h"
+
+using namespace std;
+
+struct TelemetryData {
+    float lat = 0, lon = 0, alt = 0, acc = 0;
+    int rsrp = 0;
+    string type = "";
+    mutex mtx; 
+    string raw = "";
+} g_data;
+
+
+string find_val(string json_text, string key) {
+    string stroka_poiska = "\"" + key + "\":";
+    size_t start_index = json_text.find(stroka_poiska);
+
+    if (start_index == string::npos) { //если слово не нашлось
+        return "0";
+    }
+
+    size_t value_start = start_index + stroka_poiska.length();
+
+    if (json_text[value_start] == '\"') {
+        value_start++;
+    }
+
+    size_t value_end = json_text.find_first_of("\",}]", value_start);
+
+    if (value_end == string::npos) {
+        return "0";
+    }
+
+    string result = json_text.substr(value_start, value_end - value_start);
+    return result;
+}
+
+
+
+void zmq_server() {
+    zmq::context_t ctx(1);
+    zmq::socket_t sock(ctx, zmq::socket_type::rep);
+    sock.bind("tcp://*:25565");
+
+    while (true) {
+        zmq::message_t msg;
+        if (sock.recv(msg, zmq::recv_flags::none)) {
+            string raw(static_cast<char*>(msg.data()), msg.size());
+            
+            lock_guard<mutex> lock(g_data.mtx);
+            g_data.lat = stof(find_val(raw, "Latitude"));
+            g_data.lon = stof(find_val(raw, "Longitude"));
+            g_data.alt = stof(find_val(raw, "Altitude"));
+            g_data.acc = stof(find_val(raw, "Accuracy"));
+            g_data.rsrp = stoi(find_val(raw, "RSRP"));
+            g_data.type = find_val(raw, "Type");
+            g_data.raw = raw;
+
+            sock.send(zmq::str_buffer("OK"), zmq::send_flags::none);
+        }
+    }
+}
+
+int main(int argc, char *argv[]) {
+    thread(zmq_server).detach();
+
+    SDL_Init(SDL_INIT_VIDEO | SDL_INIT_TIMER);
+    SDL_Window* window = SDL_CreateWindow(
+        "Backend start", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED,
+        1024, 768, SDL_WINDOW_OPENGL | SDL_WINDOW_RESIZABLE);
+    SDL_GLContext gl_context = SDL_GL_CreateContext(window);
+
+
+    ImGui::CreateContext();
+    ImPlot::CreateContext();
+
+    ImGuiIO& io = ImGui::GetIO(); (void)io;
+    io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard;
+    io.ConfigFlags |= ImGuiConfigFlags_DockingEnable;
+
+    ImGui_ImplSDL2_InitForOpenGL(window, gl_context);
+    ImGui_ImplOpenGL3_Init("#version 330");
+
+
+
+
+
+    bool running = true;
+    while (running) {
+        SDL_Event event;
+        while (SDL_PollEvent(&event)) {
+            ImGui_ImplSDL2_ProcessEvent(&event);
+            if (event.type == SDL_QUIT) running = false;
+        }
+
+
+        ImGui_ImplOpenGL3_NewFrame();
+        ImGui_ImplSDL2_NewFrame();
+        ImGui::NewFrame();
+        ImGui::DockSpaceOverViewport(0, nullptr, ImGuiDockNodeFlags_None);
+
+        {
+            static int counter = 0;
+            ImGui::Begin("smartphone data"); 
+                
+                ImGui::Text("data from smartphone received:");
+                ImGui::Separator();
+
+
+                {
+                    lock_guard<mutex> lock(g_data.mtx);
+                    ImGui::Text("Latitude:  %.6f", g_data.lat);
+                    ImGui::Text("Longitude: %.6f", g_data.lon);
+                    ImGui::Text("Altitude:  %.2f m", g_data.alt);
+                    ImGui::Text("Accuracy:  %.1f m", g_data.acc);
+                    ImGui::Separator();
+                    ImGui::Text("Net Type:  %s", g_data.type.c_str());
+                    ImGui::Text("RSRP:      %d dBm", g_data.rsrp);
+                    ImGui::TextUnformatted(g_data.raw.c_str());
+                }
+
+                ImGui::Separator();
+                if (ImGui::Button("Button")) counter++;
+                ImGui::Text("counter = %d", counter);
+            ImGui::End();
+        }
+
+        ImGui::Render();
+        glClearColor(0.1f, 0.1f, 0.1f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
+        ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
+        SDL_GL_SwapWindow(window);
+    }
+
+
+
+
+
+
+
+
+
+    ImGui_ImplOpenGL3_Shutdown();
+    ImGui_ImplSDL2_Shutdown();
+    ImPlot::DestroyContext();
+    ImGui::DestroyContext();
+    SDL_GL_DeleteContext(gl_context);
+    SDL_DestroyWindow(window);
+    SDL_Quit();
+
+    return 0;
+}
